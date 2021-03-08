d1db4031573f6a852ab1a59b4c7bf77b8be47f2c
Rafael Dias <rdmeneze@gmail.com>
2021-03-08 17:32:50 +0100

    exercises week 4

--- Hardware Description Languages for FPGA Design/week4/AAC2M4P3/AAC2M4P3.v
+++ Hardware Description Languages for FPGA Design/week4/AAC2M4P3/AAC2M4P3.v
@@ -1,6 +1,48 @@
-module FSM(
-  input In1,
-  input RST,
-  input CLK, 
-  output reg Out1
-);
+module FSM( input In1, RST, CLK, output reg Out1); 
+  integer state; 
+  parameter a=0; 
+  parameter b=1; 
+  parameter c=2; 
+  always @ (posedge CLK) begin
+    if (RST==1) begin state = a; Out1 = 0; end 
+    else 
+    begin 
+      case (state) 
+        a: 
+        if (In1==1) 
+        begin 
+          state = b; 
+          Out1=0; 
+        end 
+        else 
+        begin 
+          state = a; 
+          Out1=0; 
+        end 
+        
+        b: 
+        if (In1==0) 
+        begin 
+          state = c; 
+          Out1=0; 
+        end 
+        else 
+        begin 
+          state = b; 
+          Out1=0; 
+        end 
+        
+        c: 
+        if (In1==0) 
+        begin 
+          state = a; Out1=1; 
+        end 
+        else 
+        begin 
+          state = c; Out1=0; 
+        end 
+        default: state = a; 
+      endcase 
+    end 
+  end 
+endmodule
